a3d8e6051fcaa1e630754ce537515756297c7305
Sebastiaan Tas <sj_zakelijk@hotmail.com>
2024-09-24 20:18:55 +0200

    functies toegevoegd

---
+++ reclame.py
@@ -0,0 +1,33 @@
+def aanbieding_1(smaak,prijs,korting):
+    prijs_na_korting = prijs * (0.1 korting)
+    prijs = "4"
+    smaak = "aardbei"
+
+    uitvoer = f"Vandaag in de aanbieding: emmertje ijs (1 liter) in de {smaak}, van {prijs} euro voor {prijs_na_korting} euro."
+    return uitvoer
+
+def inkomsten_totaal(inkomsten):
+    inkomsten = [220, 430, 125, 160, 205, 90, 345]
+    btw = 0.09
+    return inkomsten * 2
+
+    uitvoer = f"Het totaal van alle inkomsten van deze week is {inkomsten} euro, waarover {btw}} euro btw betaald dient te worden."
+    return uitvoer
+
+def laag_en_hoog(mijn_lijst):
+    max(inkomsten_totaal)
+    min(inkomsten_totaal)
+
+def gemiddelde(mijn_lijst)
+    gemiddelde(inkomsten_totaal) 
+    uitvoer = f"De gemiddelde inkomsten deze week zijn{mijn_lijst}"
+
+def meervoudig(invoer_lijst)
+    invoer_lijst = [10,5,3,2,1,2,9]
+
+def laag_en_hoog()
+    invoer_lijst = [10,5,3,2,1,2,9]
+
+def combinatie()
+    invoer_lijst_2
+    
